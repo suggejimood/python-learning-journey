41db145c0c7ee784a7dd1f48fbd0891ed9caadc9
Mehmet TAN aka B1-66ER <ce.mehmettan@gmail.com>
2025-06-07 00:57:50 +0300

    feat: String functions

---
+++ string-functions.py
@@ -0,0 +1,34 @@
+s = "Merhaba"
+
+
+print(s.upper())
+
+print(s.lower())
+
+
+print(s.title())
+
+print(s.capitalize())
+
+s = "Merhaba Dünya"
+print(s.strip())
+
+print(s.replace("a", "q"))
+
+print(s.find("a"))
+
+print(s.startswith("Me"))
+
+print(s.endswith("cs"))
+
+name = "Mehmet"
+surname = "TAN"
+
+print(f"Ad: {name} Soyad: {surname}")
+print("Ad: {} Soyad: {} Yaş: {}".format(name, surname, 27))
+
+multi = """Bu bir çoklu satır denemesi.
+Birden fazla satır böyle oluyor.
+Güzel.
+"""
+print(multi)
