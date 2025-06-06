140a1b0e767114aae0efd1fff82663cb49764621
Mehmet TAN aka B1-66ER <ce.mehmettan@gmail.com>
2025-06-07 00:57:25 +0300

    feat: loop

---
+++ loop.py
@@ -0,0 +1,15 @@
+for i in range(5):
+    print(i)
+
+i = 0
+while i < 5:
+    print(i)
+    i +=1
+    
+for i in range(3):
+    if i == 5:
+        break
+else:
+    print("Kırılmadan bitti")
+
+    
