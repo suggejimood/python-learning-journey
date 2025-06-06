7d3169fc89757e309c35a8fc7f235b4ab70d9c32
Mehmet TAN aka B1-66ER <ce.mehmettan@gmail.com>
2025-06-07 00:58:01 +0300

    feat: Operatiors

---
+++ opretiors.py
@@ -0,0 +1,32 @@
+print(6 + 5) #toplama
+print(6 - 5) #çıkarma
+print(6*5) #çarpma
+print(10/5) # bölme
+print(5//2) #tam bölme
+print(5%2) #Mod
+print(5**2) #Üzeri
+
+x = 3
+
+x += 1 #x = x + 1
+x *= 2 #x = x * 2
+x %= 3 #x = x % 3
+
+print(x == 4)
+print(x != 4)
+print(x < 4)
+print(x > 4)
+print(x >= 4)
+print(x <= 4)
+
+print(x in [1,2,3])
+print(str(x) not in "abc")
+print(x is 5)
+print(x is not 5)
+
+#Bit düzeyinde
+print(5 & 3) #And
+print(5 ^ 3) #xor
+print(~5) #not
+print(2<<1) #bit sola kaydır
+print(4>>1) #bit sağa kaydır
